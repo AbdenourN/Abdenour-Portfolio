66221a2aa9a21d90e038b6394fd35b20ca31e791
Abdenour N <119542260+AbdenourN@users.noreply.github.com>
2023-12-26 17:53:40 +0100

    Update Projects.js
    MAJ LIEN projet

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,6 +12,8 @@ import bitsOfCode from "../../Assets/Projects/python.png";
 import eloq from "../../Assets/Projects/eloquence.png"
 import mariage from "../../Assets/Projects/mariage.jpg"
 import dicho from "../../Assets/Projects/dicho.jpg"
+import "./tt.css";
+
 function Projects() {
   return (
     <Container fluid className="project-section">
